331cf691063f4864be4d8a84a84b04509cdc698f
Fadhl Nader <>
2024-08-29 20:43:00 +0200

    Xcode project Init

---
+++ TechnicalTest/TechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Fadhl Nader on 29/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
